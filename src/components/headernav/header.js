c0fa10f3b144b4bfa9a09a742bd93af20668b773
hugoclaude <hugocayeros@gmail.com>
2021-01-11 13:02:21 -0800

    built out layout grid and header navbar components'

---
+++ src/components/headernav/header.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+        return <div className="header">
+            <img src='http://via.plaveholder.com/50x50' /> 
+        </div>;
+    }
+}
+
+export default Header;
