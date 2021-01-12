17fd3fed23320e4a88df9a23a174eed10a02dca2
hugoclaude <hugocayeros@gmail.com>
2021-01-12 11:02:45 -0800

    setup redux form

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,24 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter } from "react-router-dom";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Router, Switch, Route } from "react-router-dom";
 import reducers from "./reducers";
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware()(compose((window.devTooldsExtention ? window.devTooldsExtention() : f => f)(createstore)));
 
 import "./style/main.scss";
 
+import history from './history';
+
 import Layout from "./components/layout";
+import Signin from './components/auth/signin';
+import Signup from './components/auth/signup';
 
 function main() {
   ReactDOM.render(
     <Provider store={createStoreWithMiddleware(reducers)}>
-      <BrowserRouter>
+      <Router history={history}>
         <Layout>
-            
+          <Switch>
+              <Route path='/' exact component={Signin}/>
+              <Route path='/signin' exact component={Signin}/>
+              <Route path='/signup' exact component={Signup}/>
+          </Switch>  
         </Layout>
-      </BrowserRouter>
+      </Router>
     </Provider>,
     document.querySelector(".app-wrapper")
   );
--- src/components/headernavbar/header.js
+++ src/components/headernavbar/header.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Header extends Component {
     render() {
         return <div className="header">
-            <img src='http://via.plaveholder.com/50x50' /> 
+            <img src='http://via.placeholder.com/50x50' /> 
         </div>;
     }
 }
