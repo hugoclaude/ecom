4f8ebe8856c1c944374f0682fe205f2815c60735
hugoclaude <hugocayeros@gmail.com>
2021-01-18 13:51:19 -0800

    built form button component in formfields.js and added submit/login button to signin form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -3,10 +3,15 @@ import React, { Component } from "react";
 import SignInForm from "./signinForm";
 
 class SignIn extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields);
+    }
+
     render() {
         return (
-            <div className="sign-in__form">
-                <SignInForm />
+            <div className="sign-in">
+                <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
         );
     }
--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -2,29 +2,35 @@ import React, { Component } from "react";
 
 import { reduxForm, Field } from "redux-form";
 
-import { FormInput } from "../formFields";
+import { FormInput, FormButton } from "../formFields";
 
 class SignInForm extends Component {
     render() {
-        const { className } = this.props;
+        const { className, handleSubmit } = this.props;
         return (
-            <form className={`${className} sign-in-form`}>
+            <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
                 <Field
                     className="sign-in-form__email"
                     name="email"
                     type="email"
                     title="Email"
                     placeholder="Email"
-                    component={FormInput}
-                />
+                    component={FormInput}/>
                 <Field
                     className="sign-in-form__password"
                     name="password"
                     type="password"
                     title="Password"
                     placeholder="Password"
-                    component={FormInput}
-                />
+                    component={FormInput}/>
+                <Field
+                    className="sign-in-form__login"
+                    onClick={() => console.log('tryna submit')}
+                    type="submit"
+                    name="login"
+                    type="login"
+                    title="login"
+                    component={FormButton}/>
             </form>
         );
     }
