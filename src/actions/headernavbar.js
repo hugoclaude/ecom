178ed3146c40eeef2f8a012ce8f756e67bb7460d
hugoclaude <hugocayeros@gmail.com>
2021-02-01 08:45:48 -0800

    developed navbar and header actions for setting new links and ised in account.js

---
+++ src/actions/headernavbar.js
@@ -0,0 +1,18 @@
+import {
+    SET_HEADER_LINKS,
+    SET_NAVBAR_LINKS
+} from './types';
+
+export function setHeaderLinks(links) {
+    return ({
+        type: SET_HEADER_LINKS,
+        payoad: links
+    })
+}
+
+export function setNavbarLinks(links) {
+    return ({
+        type: SET_NAVBAR_LINKS,
+        payoad: links
+    })
+}
