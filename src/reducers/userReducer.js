2b635dafae0a5971a4d7e4a44d9463029887dc1e
hugoclaude <hugocayeros@gmail.com>
2021-04-06 11:35:38 -0700

    fixed cart incrementing bug

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -24,13 +24,13 @@ const INITIAL_STATE = {
 export default function(state = INITIAL_STATE, action) {
     switch (action.type) {
         case ADD_CART_PRODUCT:
-            var exists = false;
+            var exists = false
             const newCP = action.payload;
             var cartProducts = []
             state.cartProducts.map(cartProduct => {
-                if(cartProduct.product.id == newCP._id) {
+                if(cartProduct.product._id == newCP._id) {
                     exists = true
-                    cartProduct.quantity +=1;
+                    cartProduct.quantity += 1;
                 }
                 cartProducts.push(cartProduct);
             })
@@ -41,7 +41,6 @@ export default function(state = INITIAL_STATE, action) {
                     quantity: 1
                 })
             }
-
             return {
                 ...state,
                 cartProducts: cartProducts
