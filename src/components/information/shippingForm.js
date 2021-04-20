03cd0345d6ffacf930962d8070e7c357f0d6c15d
hugoclaude <hugocayeros@gmail.com>
2021-04-20 10:06:17 -0700

    built order summary component, info title and underlined title components

--- src/components/information/shippingForm.js
+++ src/components/information/shippingForm.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 
-import { reduxForm, Field } from "redux-form";
+import { reduxForm, Field } from 'redux-form';
 
-import { FormInput, FormButton } from "../formFields";
+import { FormInput, FormButton } from '../formFields';
 
-import history from "../../history";
+import history from '../../history';
 
-// import OrderSummary from './orderSummary';
+import OrderSummary from './orderSummary';
 
 class ShippingForm extends Component {
     render() {
@@ -14,60 +14,60 @@ class ShippingForm extends Component {
 
         return (
             <form onSubmit={handleSubmit} className={`${className} shipping-form`}>
-                <Field className="shipping-form__name"
-                type="name"
-                title="Name"
-                placeholder="Name"
-                name="name"
+                <Field className='shipping-form__name'
+                type='name'
+                title='Name'
+                placeholder='Name'
+                name='name'
                 component={FormInput}/>
-                <Field className="shipping-form__address"
-                type="address"
-                title="Street Address"
-                placeholder="Street Address"
-                name="address"
+                <Field className='shipping-form__address'
+                type='address'
+                title='Street Address'
+                placeholder='Street Address'
+                name='address'
                 component={FormInput}/>
-                <Field className="shipping-form__city"
-                type="city"
-                title="City"
-                placeholder="City"
-                name="city"
+                <Field className='shipping-form__city'
+                type='city'
+                title='City'
+                placeholder='City'
+                name='city'
                 component={FormInput}/>
-                <Field className="shipping-form__state"
-                type="state"
-                title="State
-                placeholder="
+                <Field className='shipping-form__state'
+                type='state'
+                title='State
+                placeholder='
                 State
-                name="state"
+                name='state'
                 component={FormInput}/>
-                <Field className="shipping-form__zipcode"
-                type="zipcode"
-                title="Zipcode"
-                placeholder="Zipcode"
-                name="zipcode"
+                <Field className='shipping-form__zipcode'
+                type='zipcode'
+                title='Zipcode'
+                placeholder='Zipcode'
+                name='zipcode'
                 component={FormInput}/>
 
-                <div className="shipping-form__line"></div>
-                <Field className="shipping-form__use-this-address"
-                onClick={() => history.push("/information/payment")}
-                type="submit"
-                title="Use This Address"
-                name="use-this-address"
+                <div className='shipping-form__line'></div>
+                <Field className='shipping-form__use-this-address'
+                onClick={() => history.push('/information/payment')}
+                type='submit'
+                title='Use This Address'
+                name='use-this-address'
                 component={FormButton}/>
-                <Field className="shipping-form__back"
-                onClick={() => history.push("/signin")}
-                type="button"
-                title="Back"
-                name="back"
+                <Field className='shipping-form__back'
+                onClick={() => history.push('/signin')}
+                type='button'
+                title='Back'
+                name='back'
                 short={true}
                 component={FormButton}/>
-                {/* <OrderSummary className='shipping-form__order-summary' /> */}
+                <OrderSummary className='shipping-form__order-summary' />
             </form>
         );
     }
 }
 
 ShippingForm = reduxForm({
-    form: "ShippingForm",
+    form: 'ShippingForm',
 })(ShippingForm);
 
 export default ShippingForm;
